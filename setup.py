6bba78b993b0412f9534caf4a831d81508ab21e4
funny lab <113065276+labfunny@users.noreply.github.com>
2024-02-28 15:43:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=['requests'],
-    url='https://github.com/yourusername/tonapi',
+    url='https://github.com/labfunny/jetton-price-api',
     license='MIT',
     author='Max',
     description='A Python library for interacting with the Tonapi API',
